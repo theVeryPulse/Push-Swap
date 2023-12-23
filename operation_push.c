e71b14e23aac1b17d284150f92a5440f5c099542
Philip Li <macpilot23@outlook.com>
2023-12-23 00:37:15 +0000

    Minor refacotr

--- operation_push.c
+++ operation_push.c
@@ -12,25 +12,7 @@
 
 #include "push_swap.h"
 
-static void	try_pop_and_push(t_cdl_list **src_list, t_cdl_list **dst_list);
-
-/* pa (push a): Take the first element at the top of b and put it at the top
-   of a. Do nothing if b is empty. */
-void	pa(t_cdl_list **top_a, t_cdl_list **top_b)
-{
-	ft_putendl_fd("pa", STDOUT_FILENO);
-	try_pop_and_push(top_b, top_a);
-}
-
-/* pb (push b): Take the first element at the top of a and put it at the top
-   of b. Do nothing if a is empty. */
-void	pb(t_cdl_list **top_a, t_cdl_list **top_b)
-{
-	ft_putendl_fd("pb", STDOUT_FILENO);
-	try_pop_and_push(top_a, top_b);
-}
-
-static void	try_pop_and_push(t_cdl_list **src_list, t_cdl_list **dst_list)
+/* static void	try_pop_and_push(t_cdl_list **src_list, t_cdl_list **dst_list)
 {
 	t_cdl_list	*node_to_push;
 
@@ -38,4 +20,24 @@ static void	try_pop_and_push(t_cdl_list **src_list, t_cdl_list **dst_list)
 		return ;
 	node_to_push = list_pop(src_list);
 	list_push(dst_list, node_to_push);
+} */
+
+/* pa (push a): Take the first element at the top of b and put it at the top
+   of a. Do nothing if b is empty. */
+void	pa(t_cdl_list **top_a, t_cdl_list **top_b)
+{
+	if (!top_b || !(*top_b))
+		return ;
+	ft_putendl_fd("pa", STDOUT_FILENO);
+	list_push(top_a, list_pop(top_b));
+}
+
+/* pb (push b): Take the first element at the top of a and put it at the top
+   of b. Do nothing if a is empty. */
+void	pb(t_cdl_list **top_a, t_cdl_list **top_b)
+{
+	if (!top_a || !(*top_a))
+		return ;
+	ft_putendl_fd("pb", STDOUT_FILENO);
+	list_push(top_b, list_pop(top_a));
 }
